5c030f05977a9fc709b5c8144f60257dcdb6e030
Orosergio <34399733+Orosergio@users.noreply.github.com>
2021-04-11 02:58:42 -0600

    Mantenimientos

--- MODULO_HRM/EJECUTABLE_HRM/EJECUTABLE_HRM/Program.cs
+++ MODULO_HRM/EJECUTABLE_HRM/EJECUTABLE_HRM/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CapaVistaHRM.MDI;
+using CapaVistaHRM.Sergio.Mantenimientos;
 
 namespace EJECUTABLE_HRM
 {
@@ -17,7 +18,7 @@ namespace EJECUTABLE_HRM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMDI());
+            Application.Run(new CapaVistaHRM.Sergio.Mantenimientos.frmCurso("JLOPEZ"));
         }
     }
 }
--- MODULO_HRM/HRM/SeguridadNavegador/Navegador/CapaModelo/clsConexion.cs
+++ MODULO_HRM/HRM/SeguridadNavegador/Navegador/CapaModelo/clsConexion.cs
@@ -12,7 +12,7 @@ namespace CapaModeloNavegador
         public OdbcConnection conexion()
         {
             //creacion de la conexion via ODBC
-            OdbcConnection conn = new OdbcConnection("Dsn=Predeterminado");
+            OdbcConnection conn = new OdbcConnection("Dsn=clc_erp");
             try
             {
                 conn.Open();
